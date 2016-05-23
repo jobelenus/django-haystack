0ce4914ffe79edd70d6debc9a1f891042a7a4e5b
John Obelenus <jobelenus@gmail.com>
2016-05-23 14:36:15 -0400

    undoing the removal of boost to elasticsearch

--- haystack/backends/elasticsearch_backend.py
+++ haystack/backends/elasticsearch_backend.py
@@ -135,6 +135,10 @@ class ElasticsearchSearchBackend(BaseSearchBackend):
         current_mapping = {
             'modelresult': {
                 'properties': field_mapping,
+                '_boost': {
+                    'name': 'boost',
+                    'null_value': 1.0
+                }
             }
         }
 
